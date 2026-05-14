fa3d0e2b1aec72de8c622c457390e32322588b98
福田虎之介 <fukudatoranosuke@fukudatoranosukenoMacBook-Air.local>
2026-05-14 17:27:39 +0900

    fix port

--- app.py
+++ app.py
@@ -112,4 +112,4 @@ def handle_message(event):
 if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=5001)
+    app.run(host="0.0.0.0", port=port)
